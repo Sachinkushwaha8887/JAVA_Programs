a6e429a98d3a257e7cfcb502c89f24c99c6185ef
Sachinkushwaha8887 <sk4149972@gmail.com>
2024-02-16 22:02:49 +0530

    New code

---
+++ Greater.java
@@ -0,0 +1,12 @@
+public class Greater {
+    public static void main(String[] args) {
+        int a = 5;
+        int b= 10;
+        if(a >= b){
+            System.out.println("A is greater than b:");
+        }
+        else{
+            System.out.println("B is greater than a:");
+        }
+    }
+}
